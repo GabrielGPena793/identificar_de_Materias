93765511e2480e1ced89bb1ed7b6ae58c1be415b
GabrielGPena <kbeca.gabriel793@gmail.com>
2021-11-11 13:39:27 -0300

    primeiro envio

---
+++ js/script.js
@@ -0,0 +1,33 @@
+let input = document.querySelector("input")
+let button = document.querySelector("button")
+let resultado = document.querySelector("#response")
+
+button.addEventListener("click", event => {
+    event.preventDefault();
+
+    let codigo = input.value;
+    
+    if(codigo == "1021CDTEIWI01BRED0821FT"){
+        resultado.innerHTML = "<p> Testing I </p>"
+        resultado.classList.add("response") 
+    }else if(codigo == "021CDTWCWI01BRED0821FT") {
+        resultado.innerHTML = "<p> Teamwork & Collaboration </p>"
+        resultado.classList.add("response") 
+    }else if(codigo == "1021CDPOOWI01BRED0821FT") {
+        resultado.innerHTML = "<p> Programação Orientada à Objetos </p>"
+        resultado.classList.add("response") 
+    }else if(codigo == "1021CDINFWI01BRED0821FT") {
+        resultado.innerHTML = "<p> Infraestrutura </p>"
+        resultado.classList.add("response") 
+    }else if(codigo == "1021CDFEIIWI01BRED0821FT") {
+        resultado.innerHTML = "<p> Front End II </p>"
+        resultado.classList.add("response") 
+    }else if(codigo == "1021CDDTWI01BRED0821FT") {
+        resultado.innerHTML = "<p> Design Thinking </p>"
+        resultado.classList.add("response") 
+    }
+
+      
+})
+
+
